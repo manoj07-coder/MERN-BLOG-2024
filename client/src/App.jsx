e1e84055393bcacbd0ac87039fbc35a0a27738fa
Manoj <manojgowdacs35@gmail.com>
2024-02-01 23:28:27 +0530

    bug fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,18 +6,25 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
+import FooterCom from './components/FooterCom'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/sign-up' element={<SignUp />} />
+      <Route element={<PrivateRoute />} >
       <Route path='/dashboard' element={<Dashboard />} />
+      </Route>
       <Route path='/projects' element={<Projects />} />
     </Routes>
+    <FooterCom />
     </BrowserRouter>
   )
 }
