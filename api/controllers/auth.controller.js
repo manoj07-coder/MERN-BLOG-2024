561e1f260091f62457634a82886bb7f3b61cba6f
Manoj <manojgowdacs35@gmail.com>
2024-02-01 23:28:02 +0530

    completed user authorization

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -50,3 +50,32 @@ export const signIn = async (req,res,next)=>{
     return next(error)
    }
 }
+
+export const google = async(req,res,next)=>{
+    const {name,email,photoURL} = req.body;
+    try {
+        const user = await User.findOne({email});
+        if(user){
+            const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+            const {password:pass,...rest} = user._doc;
+            res.status(200).cookie('access_token',token,{httpOnly:true}).json(rest)
+        }else{
+            const password = Math.random().toString(36).slice(-8) + 
+            Math.random().toString(36).slice(-8);
+            const hashedPassword = bcryptjs.hashSync(password,10);
+            
+            const newUser = new User({
+                username:name.split(' ').join('') + Math.random().toString(9).slice(-4),
+                email,
+                password:hashedPassword,
+                profilePicture:photoURL
+            });
+            await newUser.save();
+            const token = jwt.sign({id:newUser._id},process.env.JWT_SECRET);
+            const {password:pass,...rest} = newUser._doc;
+            res.status(200).cookie('access_token',token,{httpOnly:true}).json(rest);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
